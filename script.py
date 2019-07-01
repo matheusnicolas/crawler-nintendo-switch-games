43c51005eac2d662fb3aedc50e9a183748cc5f28
matheus.nicolas <matheus.nicolas@virtus.ufcg.edu.br>
2019-07-01 19:05:06 -0300

    code changes, initial part of regions, gitignore and requirements

--- script.py
+++ script.py
@@ -2,25 +2,38 @@ import pandas as pd
 import requests, csv, json
 from bs4 import BeautifulSoup
 STATUS_CODE_OK = 200
-URL = "https://nintypricer.com"
+URL = "https://nintypricer.com/" # => "https://nintypricer.com/{region}"
+REGIONS = ["at", "au", "be", "bg", "ca", "ch", "cy", "cz", "de", "dk",
+            "ee", "es", "fi", "fr", "gb", "gr", "hr", "hu", "ie", "it",
+            "lt", "lu", "lv", "mt", "mx", "nl", "no", "nz", "pl", "pt",
+            "ro", "ru", "se", "si", "sk", "za"]
+JSON_NAME = "nintendo_switch_games_EUA.json"
 
 request = requests.get(URL)
 
-if(request.status_code == STATUS_CODE_OK):
-    content = request.content
-    soup = BeautifulSoup(content, "html.parser")
-    table = soup.find(name="table", attrs={'id':'datatable'})
-    
-    table_str = str(table)
-    df = pd.read_html(table_str)[0]    
-    # file = open("nintendo_switch_games_EUA.txt", "w")
+def extract_page():
+    if(request.status_code == STATUS_CODE_OK):
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        table = soup.find(name="table", attrs={'id':'datatable'})
+        table_str = str(table)
+    return table_str
+
+def populate_json(table_str):
     data = []
+    pd.read_html(table_str)[0]
     for i in pd.read_html(table_str):
         for x in range (len(i.Title)):
             data.append({
                 'game': i.Title[x].encode('utf-8'), 
                 'price': i.eShopPrice[x]
             })
-    
-    with open('nintendo_switch_games_EUA.json', 'w') as outfile:
+    return data
+
+def output_json(data):
+    with open(JSON_NAME, 'w') as outfile:
         json.dump(data, outfile, indent=2)
+
+html = extract_page()
+data = populate_json(html)
+output_json(data)
